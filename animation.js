3206148a76ae090fa348d81016afbbe535d2e4bc
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-03-17 23:58:39 -0300

    ScrollMagic removed

--- animation.js
+++ animation.js
@@ -23,41 +23,4 @@ tl.from(".product-title", 2,
         }, "-=1.9")
 
 
-//Scroll Magic//
-//------Scene 1-------//
-const controller = new ScrollMagic.Controller();
-
-const tl1 = new TimelineMax();
-tl1.from(".product-info-p2", 3,
-    {
-        autoAlpha: 0,
-        ease: Power2.easeInOut
-    })
-    .from(".product-ingridients", 3,
-        {
-            autoAlpha: 0,
-            ease: Power2.easeInOut
-        })
-    .from(".product-social", 3,
-        {
-            autoAlpha: 0,
-            ease: Power2.easeInOut
-        })
-    .from(".next-products", 3,
-        {
-            autoAlpha: 0,
-            ease: Power2.easeInOut
-        })
-
-let scene1 = new ScrollMagic.Scene({
-    triggerElement: ".product-info",
-    triggerHook: .8,
-    offset: 0
-})
-    .setTween(tl1)
-    .addIndicators()
-    .offset(100)
-    .duration("100%")
-    .triggerHook(.7)
-    .addTo(controller);
 
